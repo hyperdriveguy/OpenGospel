8dad2b475ec7760bdaaaeba8cd16d5043cab7cb1
Hyperdriveguy <hyperdriveguy@gmail.com>
2016-06-27 15:26:34 -0700

    Rebase OpenGospel on Rosoklana Web Browser
    Starting implementation of bookmarks, now using glade for interface,
    relicense to GNU GPL v3.

--- scriptures.py
+++ scriptures.py
@@ -1,60 +1,92 @@
 #!/usr/bin/python
-import gtk, webkit
+import os, sys, gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('WebKit', '3.0')
+
+from gi.repository import Gtk, WebKit
+
+# Working directory - change this to where OpenGospel is
+wrk_dir = '/home/carson/Programming/OpenGospel'
 
 # OpenGospel version
-ver = "v0.0.1"
+ver = "v0.1-dev"
 
-# Main Window
-class MainWindow():
+class MainWindow:
+  def __init__(self):
+    self.builder = Gtk.Builder()
+   
+    self.builder.add_from_file("scriptures.glade")
+    self.builder.connect_signals(self)
 
-    def __init__(self):
+    self.toolbar1 = self.builder.get_object("navbar")
+    self.back = self.builder.get_object("back")
+    self.forward = self.builder.get_object("forward")
+    self.refresh = self.builder.get_object("home")
+    self.bookmark_button = self.builder.get_object("bookmark")
+    self.window = self.builder.get_object("scriptures")
+    self.window.connect('destroy', lambda w: Gtk.main_quit())
+    self.scrolledwindow = self.builder.get_object("scrolledwindow")
+    self.window.show_all()
+
+    self.webview = WebKit.WebView()
+    self.scrolledwindow.add(self.webview)
+    self.webview.open('file:' + wrk_dir + '/scriptures.nephi.org/docbook/index.html')
+    self.webview.connect('title-changed', self.change_title)
+    self.webview.connect('load-committed', self.change_current_url)
+    self.webview.show()	
+   
+
+  def on_home_clicked(self, widget):
+    self.webview.open('file:' + wrk_dir + '/scriptures.nephi.org/docbook/index.html')
+
+  def on_back_clicked(self, widget):
+    self.webview.go_back()
+    
+  def on_forward_clicked(self, widget):
+     self.webview.go_forward()
+     
+  def on_bookmark_clicked(self, widget):
+     bookmark_it()
+
+  def change_title(self, widget, frame, title):
+	self.window.set_title(title + " - OpenGospel " + ver)
+     
+  def change_current_url(self, widget, frame):
+     current_url = frame.get_uri()
+     self.back.set_sensitive(self.webview.can_go_back() )
+     self.forward.set_sensitive(self.webview.can_go_forward() )
+ 
+class Bookmarks:
+	def _init_(self):
+		self.builder = Gtk.Builder()
+   
+		self.builder.add_from_file("bookmarks.glade")
+		self.builder.connect_signals(self)
+
+		self.box1 = self.builder.get_object("box1")
+		self.box2 = self.builder.get_object("box2")
+		self.delete_button = self.builder.get_object("delete")
+		self.add_button = self.builder.get_object("add")
+		self.load_button = self.builder.get_object("load")
+		self.bookmark_choose = self.builder.get_object("bookmark_choose")
+		self.bookmark_window = self.builder.get_object("bookmarks")
+		self.bookmark_list = self.builder.get_object("bookmark_list")
+		self.bookmark_window.show_all()
 		
-        # Create window
-        self.window = gtk.Window()
-        self.window.connect('destroy', lambda w: gtk.main_quit())
-        self.window.set_default_size(1024, 768)
+		if self.window.connect('destroy'):
+			self.bookmark_window.hide_all()
+ 
+	def load_bookmarks():
+		print("Need help implementing this")
+  
+def bookmark_it():
+	Bookmarks()
 
-        # Create navigation bar
-        self.navBar = gtk.HBox()
+def main ():
+  mainApp = MainWindow()
+  Gtk.main()
 
-        self.back = gtk.ToolButton(gtk.STOCK_GO_BACK)
-        self.forward = gtk.ToolButton(gtk.STOCK_GO_FORWARD)
-        self.home = gtk.ToolButton(gtk.STOCK_HOME)
+if __name__ == "__main__":
+  main()
 
-        self.back.connect('clicked', self.go_back)
-        self.forward.connect('clicked', self.go_forward)
-        self.home.connect('clicked', self.home_page)
-
-        self.navBar.pack_start(self.back, False)
-        self.navBar.pack_start(self.forward, False)
-        self.navBar.pack_start(self.home, False)
-
-        # Create view for webpage
-        self.view = gtk.ScrolledWindow()
-        self.scriptures = webkit.WebView()
-        self.scriptures.open('file:///home/carson/Programming/OpenGospel/scriptures.nephi.org/docbook/index.html')
-        self.scriptures.connect('title-changed', self.change_title)
-        self.view.add(self.scriptures)
-
-        # Add everything and initialize
-        self.container = gtk.VBox()
-        self.container.pack_start(self.navBar, False)
-        self.container.pack_start(self.view)
-
-        self.window.add(self.container)
-        self.window.show_all()
-        gtk.main()
-
-    def change_title(self, widget, frame, title):
-        self.window.set_title(title + " - OpenGospel " + ver)
-
-    def go_back(self, widget):
-        self.scriptures.go_back()
-
-    def go_forward(self, widget):
-        self.scriptures.go_forward()
-
-    def home_page(self, widget):
-        self.scriptures.open('file:///home/carson/Programming/OpenGospel/scriptures.nephi.org/docbook/index.html')
-
-mainWindow = MainWindow()
