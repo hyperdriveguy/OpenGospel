de15758451fd86a14575c47bc820c5400089fac1
Carson <hyperdriveguy@gmail.com>
2017-09-04 15:13:12 -0700

    Add ability to generate a makefile and install (incomplete)

--- scriptures.py
+++ scriptures.py
@@ -20,7 +20,8 @@ along with OpenGospel.  If not, see <http://www.gnu.org/licenses/>.
 import os, gi, subprocess
 
 # Only used in debugging
-#import sys
+if __debug__:
+	import sys
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
@@ -29,17 +30,22 @@ from gi.repository import Gtk, WebKit2 as WebKit
 
 # OpenGospel Constants
 # Working directory - change this to where OpenGospel is
-wrk_dir = '/home/carson/Documents/coding/opengospel'
-css_dir = wrk_dir + '/scriptures.redo/'
-config = wrk_dir + '/opengospel.conf'
+bin_dir = '/home/carson/Documents/coding/opengospel'
+share_dir = bin_dir
+css_dir = share_dir + '/scriptures.redo'
+config_dir = bin_dir
+config = config_dir + '/opengospel.conf'
 # OpenGospel version
 ver = "0.3"
 
 class ConfigInit:
 	def modecss(style):
-		subprocess.run(["rm", css_dir + "menu.css", css_dir + "scriptures.css"], check=True)
-		subprocess.run(["cp", "-T", css_dir + "themes/" + style + "menu.css", css_dir + "menu.css"], check=True)
-		subprocess.run(["cp", "-T", css_dir + "themes/" + style + "scriptures.css", css_dir + "scriptures.css"], check=True)
+		try:
+			subprocess.run(["rm", css_dir + "/menu.css", css_dir + "/scriptures.css"], check=True)
+		except FileNotFoundError:
+			pass
+		subprocess.run(["cp", "-T", share_dir + "/scriptures.redo/themes/" + style + "menu.css", css_dir + "/menu.css"], check=True)
+		subprocess.run(["cp", "-T", share_dir + "/scriptures.redo/themes/" + style + "scriptures.css", css_dir + "/scriptures.css"], check=True)
 
 	def glade_init(conf_file):
 		# Globally declare the glade file
@@ -47,13 +53,24 @@ class ConfigInit:
 		try:
 			with open(conf_file, "r") as getconf:
 				if getconf.read(1) == "T":
-					gladefile = "scriptures-csd.glade"
+					gladefile = share_dir + "/scriptures-csd.glade"
 				elif getconf.read(1) == "F":
-					gladefile = "scriptures.glade"
+					gladefile = share_dir + "/scriptures.glade"
 				else:
-					gladefile = "scriptures.glade"
+					gladefile = share_dir + "/scriptures.glade"
 		except FileNotFoundError:
-					gladefile = "scriptures.glade"
+			# First Run
+			gladefile = share_dir + "/scriptures.glade"
+			try:
+				subprocess.run(["mkdir", "-pv", config_dir], check=True)
+			except FileNotFoundError:
+				pass
+			# If no config is found, it is likely that no CSS is there either
+			subprocess.run(["cp", "-T", share_dir + "/scriptures.redo/themes/" + "normalmenu.css", css_dir + "/menu.css"], check=True)
+		subprocess.run(["cp", "-T", share_dir + "/scriptures.redo/themes/" + "normalscriptures.css", css_dir + "/scriptures.css"], check=True)
+
+		if __debug__:
+			print("UI file: " + gladefile)
 
 
 class MainWindow:
@@ -74,16 +91,20 @@ class MainWindow:
 		self.scrolledwindow = self.builder.get_object("scrolledwindow")
 		self.scriptures.show_all()
 		# Webkit
-		self.webview = WebKit.WebView.new()
+		#self.usercontentmanager = WebKit.UserContentManager.new()
+		#self.stylesheet = WebKit.UserStyleSheet.new("/home/carson/Documents/coding/opengospel/scriptures.redo/themes/nightmenu.css", WebKit.UserContentInjectedFrames(0), WebKit.UserStyleLevel(0))
+		#self.usercontentmanager.add_style_sheet(self.stylesheet)
+		#self.webview = WebKit.WebView.new_with_user_content_manager(self.usercontentmanager)
+		self.webview =WebKit.WebView.new()
 		self.scrolledwindow.add(self.webview)
-		self.webview.load_uri('file://' + wrk_dir + '/scriptures.redo/main-menu.html')
+		self.webview.load_uri('file://' + share_dir + '/scriptures.redo/main-menu.html')
 		self.webview.connect('notify::title', self.change_title)
 		self.webview.connect('load-changed', self.change_current_url)
 		self.webview.show()
 
 	def on_menu_clicked(self, widget):
 		# Statically set Main Menu
-		self.webview.load_uri('file:' + wrk_dir + '/scriptures.redo/main-menu.html')
+		self.webview.load_uri('file:' + share_dir + '/scriptures.redo/main-menu.html')
 
 	def on_last_clicked(self, widget):
 		# Go to the Previous Page
@@ -143,7 +164,8 @@ class MainWindow:
 			else:
 				self.csdswitch.set_active(False)
 				csd_on = "F"
-			#print("csd_on: " + csd_on, file=sys.stderr)
+			if __debug__:
+				print("csd_on: " + csd_on, file=sys.stderr)
 
 			nightmode_on = setconf.read(2)
 			if nightmode_on == "T":
@@ -151,7 +173,8 @@ class MainWindow:
 			else:
 				self.nightmodeswitch.set_active(False)
 				nightmode_on = "F"
-			#print("nightmode_on: " + nightmode_on, file=sys.stderr)
+			if __debug__:
+				print("nightmode_on: " + nightmode_on, file=sys.stderr)
 		else:
 			setconf = open(config, "w+")
 			csd_on = "F"
@@ -184,7 +207,8 @@ class MainWindow:
 		self.applysettings.set_sensitive(False)
 		self.cancelsettings.set_sensitive(False)
 		# For debugging the config
-		#print("Wrote: " + csd_on + nightmode_on, file=sys.stderr)
+		if __debug__:
+			print("Wrote: " + csd_on + nightmode_on, file=sys.stderr)
 		setconf.close()
 	def on_cancelsettings_clicked(self, widget):
 		setconf.close()
@@ -211,12 +235,12 @@ class MainWindow:
 		else:
 			self.next.set_sensitive(True)
 			self.previous.set_sensitive(True)
-			if current_url == 'file://' + wrk_dir + '/scriptures.redo/bom/1nephi/1.html':
+			if current_url == 'file://' + share_dir + '/scriptures.redo/bom/1nephi/1.html':
 				self.previous.set_sensitive(False)
 
 def calculate_chapter():
 	global chapter
-	chapter = current_url[current_url.index('file://' + wrk_dir):current_url.index('.html')]
+	chapter = current_url[current_url.index('file://' + share_dir + '/scriptures.redo/'):current_url.index('.html')]
 	isInt = False
 	while isInt == False:
 		try:
